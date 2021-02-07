f80e409d41ea1fabcdd97ab97f3a7fb336809e21
Cathy Ludwig <cathymay9@gmail.com>
2021-02-06 20:47:37 -0600

    trying to add automated builds

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,5 +1,5 @@
 module.exports = {
-    siteUrl: `https://cathy-ludwig.github.io`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://cathyiscool.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
