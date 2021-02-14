45d7520509368bc7fa40b5345034d997b4b7005b
Cathy Ludwig <cathymay9@gmail.com>
2021-02-13 21:39:17 -0600

    updating images to improve speed

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     description: `An Adventure in Tech`,
     title: `Cathy Ludwig`,
-    cover_image: `https://images.unsplash.com/photo-1451186859696-371d9477be93?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1652&q=80`,
+    cover_image: `images/nasa-earth.jpeg`,
     github: `cathy-ludwig`,
     icon: `favicon.png`,
     lang: `en`,
